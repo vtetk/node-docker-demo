703a67f4f19d8949b96e31a61200ab3a4f9d2eb3
vtetk <tetk@burmesecloud.com>
2021-01-08 13:45:05 +1100

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello Azure!!!');
+  res.send('Hello World 2021!!!');
 });
 
 app.listen(config.port, () => {
